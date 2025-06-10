69eb6ddcf6ea4bc1f13f4f1bc1769acc499d5878
Chadi Abi Fadel <chadi@abifadel.net>
2025-06-10 20:30:10 +0300

    adv_strings.py

---
+++ learning_material/adv_strings.py
@@ -0,0 +1,7 @@
+# Write strings that contain both single and double quotes.
+print("this is a '\" string'")
+# Print strings with embedded newlines (\n) and tabs (\t).
+print("this is a \n string with a newline")
+print("this is a \t string with a tab")
+# Use raw strings for Windows file paths.
+print(r"C:\Users\User\Documents\file.txt")
